883c723c70034c1ba04c6048cf07da24b229ef2f
Amin Mousavi <seyedamin963@gmail.com>
2024-03-04 13:28:12 +0330

    Build the navbar

---
+++ app/Navbar.tsx
@@ -0,0 +1,35 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { MdOutlineBugReport } from "react-icons/md";
+
+const Navbar = () => {
+  const currentPath = usePathname();
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
+  return (
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href="/">
+        <MdOutlineBugReport />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map((link) => (
+          <li key={link.href}>
+            <Link
+              href={link.href}
+              className={`${
+                link.href === currentPath ? "text-zinc-900" : "text-zinc-500"
+              } hover:text-zinc-800 transition-colors`}
+            >
+              {link.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
