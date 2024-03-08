160c86107264ba9f95aaeda6563b4e86daa02b8e
Amin Mousavi <seyedamin963@gmail.com>
2024-03-08 14:25:44 +0330

    Build IssueSummary to showing the total number of issues based on the status

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,23 @@
-import { Text } from "@radix-ui/themes";
-import Pagination from "./components/Pagination";
+import prisma from "@/prisma/client";
 import LatestIssues from "./LatestIssues";
+import IssueSummary from "./IssueSummary";
 
-export default function Home() {
+export default async function Home() {
+  const openIssues = await prisma.issue.count({ where: { status: "OPEN" } });
+  const inProgressIssues = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
+  const closedIssues = await prisma.issue.count({
+    where: { status: "CLOSED" },
+  });
+
   return (
     <>
+      <IssueSummary
+        open={openIssues}
+        inProgress={inProgressIssues}
+        closed={closedIssues}
+      />
       <LatestIssues />
     </>
   );
